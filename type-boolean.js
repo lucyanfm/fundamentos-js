1e8122c4284544646f15c7a40342dbadd7c3c398
lucyan miranda <lucyanprog@gmail.com>
2022-04-26 20:20:17 -0300

    exemplos da aula1

---
+++ type-boolean.js
@@ -0,0 +1,3 @@
+const n1 = 1;
+const n2 =2 ;
+console.log(n1 < n2)
---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 ='Olá, Mundo';
+const text2 ='Olá, mundo';
+const senha = 'senhaSuperSegura';
+
+const citacao = 'Meu nome é ';
+const meuNome = "Lucyan";
+
+// concatenação (+)
+console.log(citacao + meuNome)
+
+//template string ou template literal 
